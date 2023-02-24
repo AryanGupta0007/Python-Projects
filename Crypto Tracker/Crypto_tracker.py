18bd8a5179bc5b18dd6048273a07f542efbc28b9
AryanGupta0007 <aryangupta07075@gmail.com>
2023-02-24 10:27:22 +0530

    reorganizing the files

--- Crypto Tracker/Crypto_tracker.py
+++ Crypto Tracker/Crypto_tracker.py
@@ -4,7 +4,11 @@ import keyboard
 import sys
 import os
 import time
-    
+from datetime import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+from tabulate import tabulate
+
 def main():
     os.system("cls")
     print("Press \"Q\" to exit the program")
@@ -12,16 +16,23 @@ def main():
     symbol, initial_price, alert_price, notification = get_user_input()
     URL_for_dollar = f"https://www.binance.com/api/v3/ticker/price?symbol={symbol}USDT"
     URL_for_btc = f"https://www.binance.com/api/v3/ticker/price?symbol={symbol}BTC"
-    
+
     prices_dollar = []
+    times = []
     prices_btc = []
-    
+                
     os.system("cls")
     print(f"Intial Price: {round(float(initial_price), 2)} Alert Price: {notification} to {alert_price}\n\n")
-
+    
     while True:
-        
+        symbol_data = {"price":prices_dollar, "time":times}    
         get_Data_Dollar = requests.get(URL_for_dollar).json()
+        # get_current_time = datetime.now()
+        get_current_time = time.strftime("%H:%M:%S", time.localtime())
+        
+        times.append(get_current_time)
+        # df = pd.DataFrame(symbol_data)
+        # print(times, end="\r")
         # print(get_Data_Dollar)
         if (symbol != "BTC"):
             get_Data_BTC = requests.get(URL_for_btc).json()
@@ -88,6 +99,7 @@ def main():
                 print("Press \"S\" to see the price stats")         
                 
                 print(f"Symbol: {symbol}\t\t Current Price: ${round(float(price_dollar), 2)}\t\tAlert Price: {alert_price}\t Difference from alert price: ${difference}\t ", end = "\r")
+                print(tabulate(df, headers='keys', tablefmt='fancy_grid'))
                 sys.stdout.flush()
         
              # carriage return used to return the curson of the mouse to the initial position
