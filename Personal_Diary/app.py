03b0ecb8a2e7b03020d43b124bb3570d0122427d
AryanGupta0007 <aryangupta07075@gmail.com>
2023-02-26 20:14:59 +0530

    Completed Personal Diary

--- Personal_Diary/app.py
+++ Personal_Diary/app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for, session
 import sqlite3
 import datetime
 app = Flask(__name__)
 conn = sqlite3.connect('database.db', check_same_thread=False)
+app.secret_key = 'super secret key'
  
 # cursor object
 crsr = conn.cursor()
@@ -18,13 +19,15 @@ db = """ CREATE TABLE IF NOT EXISTS DATA (
             Name CHAR(25) NOT NULL,
             Password TEXT NOT NULL
         ); """
- 
+# crsr.execute("DROP TABLE DIARY") 
 diary = """CREATE TABLE IF NOT EXISTS DIARY (
+        Email CHAR(255),
         Notes CHAR(255),
         Date_time TEXT NOT NULL
         )"""
 crsr.execute(diary)        
 crsr.execute(db)
+crsr.execute("SELECT * FROM DATA")
 # # print(N)
 # output = crsr.execute("SELECT Email FROM DATA")
 # for row in output:
@@ -36,67 +39,70 @@ def homepage():
 
     return render_template("index.html")    
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if (request.method) == "POST":
+        Password = request.form.get("password")
+        Name = request.form.get("name")
+        Email = request.form.get("email")
+        crsr.execute("SELECT   Password FROM DATA  WHERE  Email = ? ", (Email,))
+        order = crsr.fetchall()
+        try:
+            for row in order:
+                for column in row:
+                    password = column
+            if (password == Password):
+                return redirect(url_for("diary", email= Email))
+            else:
+                return redirect("/login")
+        except:
+            return redirect("/register")
+        
+    elif (request.method) == "GET":
+        
+        return render_template("login.html")    
 
-    return render_template("login.html")    
-
-@app.route("/signup")
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
-
-    return render_template("signup.html")    
-
-@app.route("/diary")
-def diary():
-    old_notes = crsr.execute("SELECT * FROM DIARY WHERE ")# buggy code the data imported should be of the logged in user 
-    
-    return render_template("diary.html", old_notes=old_notes)    
-
-@app.route("/note_added", methods=["POST"])
-def notes_added():
-    notes = request.form.get("diary")
-    current_date_time = datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y")
-    crsr.execute("INSERT INTO DIARY (Notes, Date_time) VALUES  (?, ?)", (notes, current_date_time))
-    
-    return redirect("\diary")
-
-
-@app.route("/success", methods = ["POST"])
-def success_register():
-    Password = request.form.get("password")
-    Name = request.form.get("name")
-    Email = request.form.get("email")
-    try:
-        crsr.execute("INSERT INTO DATA (Email, Name, Password) VALUES (?, ?, ?)", (Email, Name, Password))
-    except:
-        return render_template("failure.html") 
+    if (request.method) == "POST":
+        Password = request.form.get("password")
+        Name = request.form.get("name")
+        Email = request.form.get("email")
+        try:
+            crsr.execute("INSERT INTO DATA (Email, Name, Password) VALUES (?, ?, ?)", (Email, Name, Password))
+        except:
+            return render_template("failure.html") 
+        else:
+            output = crsr.execute("SELECT * FROM DATA")
+            for row in output:
+                print(row)
+        
+            return redirect(url_for("diary", email= Email))
     else:
-        output = crsr.execute("SELECT * FROM DATA")
-        for row in output:
-            print(row)
+        return render_template("signup.html")    
+
+@app.route("/diary", methods = ["GET", "POST"])
+def diary():
     
-        return render_template("success.html")    
+    if request.method == "GET":
+        email = request.args.get("email")
+        session["Email"] = email
+        try:
+            old_notes = crsr.execute("SELECT Notes, Date_time FROM DIARY WHERE Email = ?", (email,))# buggy code the data imported should be of the logged in user             
+            return render_template("diary.html", old_notes=old_notes)
+        except:
+            # buggy code the data imported should be of the logged in user             
+            return render_template("diary.html", old_notes="")
+            
+    elif request.method == "POST":
+        email = session.get("Email", None)
+        notes = request.form.get("diary")
+        current_date_time = datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y")
+        crsr.execute("INSERT INTO DIARY (Email, Notes, Date_time) VALUES  (?, ?, ?)", (email, notes, current_date_time))
+        old_notes = crsr.execute("SELECT Notes, Date_time FROM DIARY where email = ?", (email,))# buggy code the data imported should be of the logged in user             
+        return render_template("diary.html", old_notes=old_notes)
 
         
-@app.route("/login_success", methods = ["POST"])
-def successful_login():
-    Password = request.form.get("password")
-    # Password = list(Password)
-    Email = request.form.get("email")
-    # Password = [tuple(Password)
-    crsr.execute("SELECT   Password FROM DATA  WHERE  Email = ? ", (Email,))
-    order = crsr.fetchall()
-    try:
-        for row in order:
-            for column in row:
-                password = column
-        if (password == Password):
-            return render_template("success_login.html")
-        else:
-            return redirect("/login")
-    except:
-        return redirect("/signup")
-    # else:
         
 @app.route("/logout")
 def logout():
